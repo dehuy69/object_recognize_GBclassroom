1eae170715ffe22a19ac6850dc1f943a6ba11b4c
huyna <huyna@blueseed.tv>
2018-09-26 11:29:12 +0700

    testing

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from tensorflow import keras
 import numpy as np
+import time
 
 size = (224,224,3)
 model = keras.applications.MobileNet(input_shape=size)
+start_time = time.time()
 im = keras.preprocessing.image.load_img('test.jpeg', target_size=size)
 im = keras.preprocessing.image.img_to_array(im)
 x = np.expand_dims(im, axis=0)
@@ -10,3 +12,4 @@ x = keras.applications.mobilenet.preprocess_input(x)
 pred = model.predict(x)
 out = keras.applications.mobilenet.decode_predictions(pred)
 print (out)
+print("--- %s seconds ---" % (time.time() - start_time))
