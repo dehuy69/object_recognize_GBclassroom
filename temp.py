71b91d570e4c45d31bcbfd980d594c998dd6dce6
huyna <huyna@blueseed.tv>
2018-09-26 18:48:56 +0700

    update

--- temp.py
+++ temp.py
@@ -1,45 +1,15 @@
-import io
-import socket
-import struct
-import time
-import picamera
+import numpy as np
+import cv2
+img = cv2.imread('star.png')
+imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+image, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-# Connect a client socket to my_server:8000 (change my_server to the
-# hostname of your server)
-client_socket = socket.socket()
-client_socket.connect(('my_server', 8000))
+# img = cv2.drawContours(img, contours, -1, (0,255,0), 3)
+# img = cv2.drawContours(img, contours, 3, (0,255,0), 3)
+cnt = contours[2]
+img = cv2.drawContours(img, [cnt], 0, (0,255,0), 3)
 
-# Make a file-like object out of the connection
-connection = client_socket.makefile('wb')
-try:
-    camera = picamera.PiCamera()
-    camera.resolution = (640, 480)
-    # Start a preview and let the camera warm up for 2 seconds
-    camera.start_preview()
-    time.sleep(2)
-
-    # Note the start time and construct a stream to hold image data
-    # temporarily (we could write it directly to connection but in this
-    # case we want to find out the size of each capture first to keep
-    # our protocol simple)
-    start = time.time()
-    stream = io.BytesIO()
-    for foo in camera.capture_continuous(stream, 'jpeg'):
-        # Write the length of the capture to the stream and flush to
-        # ensure it actually gets sent
-        connection.write(struct.pack('<L', stream.tell()))
-        connection.flush()
-        # Rewind the stream and send the image data over the wire
-        stream.seek(0)
-        connection.write(stream.read())
-        # If we've been capturing for more than 30 seconds, quit
-        if time.time() - start > 30:
-            break
-        # Reset the stream for the next capture
-        stream.seek(0)
-        stream.truncate()
-    # Write a length of zero to the stream to signal we're done
-    connection.write(struct.pack('<L', 0))
-finally:
-    connection.close()
-    client_socket.close()
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
