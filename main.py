82ebf8193d38637f1b3fce1a8d4942f548765a57
huyna <huyna@blueseed.tv>
2018-09-26 14:38:36 +0700

    testing

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from tensorflow import keras
 import numpy as np
 import time
 
-size = (224,224,3)
+size = (112,112,3)
 model = keras.applications.MobileNet(input_shape=size)
-start_time = time.time()
 im = keras.preprocessing.image.load_img('test.jpeg', target_size=size)
 im = keras.preprocessing.image.img_to_array(im)
+start_time = time.time()
 x = np.expand_dims(im, axis=0)
 x = keras.applications.mobilenet.preprocess_input(x)
 pred = model.predict(x)
