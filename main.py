68a8ee3bcf98ea6e5cedb333a8969f761530c4e3
Huy Nguyen <huyna@blueseed.tv>
2018-10-01 00:48:04 +0700

    update

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ import numpy as np
 import time
 import io
 
-camera = picamera.PiCamera()
+camera = PiCamera()
+stream = PiRGBArray(camera)
 camera.start_preview(fullscreen=False, window=(100,20,800,600))
 stream = io.BytesIO()
 size = (224,224,3)
 model = keras.applications.MobileNet(input_shape=size)
 while True:
     try:
-        camera.capture(stream, format='jpeg')
-        print (stream.shape)
-        im = cv2.resize(stream, size[:2])
+        camera.capture(stream, format='bgr')
+        image = stream.array
+        im = cv2.resize(image, size[:2])
         #im = keras.preprocessing.image.load_img('test.jpeg', target_size=size)
         #im = keras.preprocessing.image.img_to_array(im)
         start_time = time.time()
