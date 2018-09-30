ee3f379c826cc56dc55f5937bfe6ead5ac5d2692
Huy Nguyen <huyna@blueseed.tv>
2018-10-01 00:34:06 +0700

    update

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import numpy as np
 import time
-camera = PiCamera()
-# camera.resolution = (800, 600)
-rawCapture = PiRGBArray(camera)
-privious_data = []
-privious_time = 0
+import io
+
+camera = picamera.PiCamera()
 camera.start_preview(fullscreen=False, window=(100,20,800,600))
+stream = io.BytesIO()
 size = (224,224,3)
 model = keras.applications.MobileNet(input_shape=size)
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+while True:
     try:
-        frame = rawCapture.array
-        print(frame.shape)
-        im = cv2.resize(frame, size[:2])
+        camera.capture(stream, format='jpeg')
+        print (stream.shape)
+        im = cv2.resize(stream, size[:2])
         #im = keras.preprocessing.image.load_img('test.jpeg', target_size=size)
         #im = keras.preprocessing.image.img_to_array(im)
         start_time = time.time()
@@ -25,7 +24,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         out = keras.applications.mobilenet.decode_predictions(pred)
         print (out)
         print("--- %s seconds ---" % (time.time() - start_time))
-        rawCapture.truncate(0)
+        stream.seek(0)
     except:
         camera.stop_preview()
         break
