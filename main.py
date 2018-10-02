990d864a049cc1d8c8b8a8f35d359219ca51f482
dehuy69 <dr.huy69@gmail.com>
2018-10-02 16:16:17 +0700

    testing

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from picamera import PiCamera
 import numpy as np
 import time
 import cv2
+from processes import predict
 
 camera = PiCamera()
 stream = PiRGBArray(camera)
 size = (224,224,3)
-model = keras.applications.MobileNet(input_shape=size)
 camera.start_preview(fullscreen=False, window=(100,20,800,600))
 while True:
     time.sleep(1)
@@ -20,9 +20,10 @@ while True:
         #im = keras.preprocessing.image.img_to_array(im)
         start_time = time.time()
         x = np.expand_dims(im.astype('float32'), axis=0)
-        x = keras.applications.mobilenet.preprocess_input(x)
-        pred = model.predict(x)
-        out = keras.applications.mobilenet.decode_predictions(pred)
+        # x = keras.applications.mobilenet.preprocess_input(x)
+        # pred = model.predict(x)
+        # out = keras.applications.mobilenet.decode_predictions(pred)
+        out = predict(x)
         print (out)
         print("--- %s seconds ---" % (time.time() - start_time))
         stream.seek(0)
